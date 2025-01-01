885c51a7da12f5dca2ff72136a787d5dc09aef06
Melih Akay <melih.akay@metu.edu.tr>
2025-01-01 03:20:52 +0300

    adaption to marcie

--- f1tenth_stack/launch/bringup_launch.py
+++ f1tenth_stack/launch/bringup_launch.py
@@ -71,12 +71,12 @@ def generate_launch_description():
 
     ld = LaunchDescription([joy_la, vesc_la, sensors_la, mux_la])
 
-    joy_node = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy',
-        parameters=[LaunchConfiguration('joy_config')]
-    )
+    # joy_node = Node(
+    #     package='joy',
+    #     executable='joy_node',
+    #     name='joy',
+    #     parameters=[LaunchConfiguration('joy_config')]
+    # )
     joy_teleop_node = Node(
         package='joy_teleop',
         executable='joy_teleop',
@@ -87,7 +87,11 @@ def generate_launch_description():
         package='vesc_ackermann',
         executable='ackermann_to_vesc_node',
         name='ackermann_to_vesc_node',
-        parameters=[LaunchConfiguration('vesc_config')]
+        parameters=[LaunchConfiguration('vesc_config')],
+        remappings=[
+            ('commands/motor/speed', 'commands/motor/unsmoothed_speed'),
+            ('commands/servo/position', 'commands/servo/unsmoothed_position')
+        ]
     )
     vesc_to_odom_node = Node(
         package='vesc_ackermann',
@@ -107,35 +111,35 @@ def generate_launch_description():
         name='throttle_interpolator',
         parameters=[LaunchConfiguration('vesc_config')]
     )
-    urg_node = Node(
-        package='urg_node',
-        executable='urg_node_driver',
-        name='urg_node',
-        parameters=[LaunchConfiguration('sensors_config')]
-    )
+    # urg_node = Node(
+    #     package='urg_node',
+    #     executable='urg_node_driver',
+    #     name='urg_node',
+    #     parameters=[LaunchConfiguration('sensors_config')]
+    # )
     ackermann_mux_node = Node(
         package='ackermann_mux',
         executable='ackermann_mux',
         name='ackermann_mux',
         parameters=[LaunchConfiguration('mux_config')],
-        remappings=[('ackermann_cmd_out', 'ackermann_drive')]
+        remappings=[('ackermann_drive_out', 'ackermann_cmd')]
     )
-    static_tf_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_baselink_to_laser',
-        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
-    )
+    # static_tf_node = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='static_baselink_to_laser',
+    #     arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+    # )
 
     # finalize
-    ld.add_action(joy_node)
+    # ld.add_action(joy_node)
     ld.add_action(joy_teleop_node)
     ld.add_action(ackermann_to_vesc_node)
     ld.add_action(vesc_to_odom_node)
     ld.add_action(vesc_driver_node)
-    # ld.add_action(throttle_interpolator_node)
-    ld.add_action(urg_node)
+    ld.add_action(throttle_interpolator_node)
+    # ld.add_action(urg_node)
     ld.add_action(ackermann_mux_node)
-    ld.add_action(static_tf_node)
+    # ld.add_action(static_tf_node)
 
     return ld
